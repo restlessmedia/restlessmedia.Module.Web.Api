1758cdfbf323c2c95ac5a23a0f762850b4123892
restlessmedia <phillycooper@hotmail.com>
2019-04-02 13:27:11 +0100

    Latest version

--- src/WebModule.cs
+++ src/WebModule.cs
@@ -2,20 +2,21 @@
 using Autofac.Integration.WebApi;
 using restlessmedia.Module.Web.Api.Attributes;
 using restlessmedia.Module.Web.Api.MediaType;
+using System.Collections.Generic;
 using System.Web.Http;
 
 namespace restlessmedia.Module.Web.Api
 {
   public class WebModule : IWebModule
   {
-    public void Init(HttpConfiguration httpConfiguration, ContainerBuilder builder)
+    public void OnStart(HttpConfiguration httpConfiguration, ContainerBuilder builder, IEnumerable<IWebModule> webModules)
     {
       httpConfiguration.Formatters[0] = new JsonNetFormatter(); // insert at 0 so it runs before any others
       httpConfiguration.MapHttpAttributeRoutes();
       httpConfiguration.Filters.Add(new ExceptionHandlingAttribute());
     }
 
-    public void Init(HttpConfiguration httpConfiguration, IContainer container)
+    public void OnStart(HttpConfiguration httpConfiguration, IContainer container, IEnumerable<IWebModule> webModules)
     {
       httpConfiguration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
     }

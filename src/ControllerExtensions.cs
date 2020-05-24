31a36d36fe2d070af5657e0263e4adb6a0dbf2a8
Philip Cooper <phillycooper@hotmail.com>
2020-05-24 05:22:57 +0100

    Controller extensions

---
+++ src/ControllerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http.Results;
+
+namespace System.Web.Http
+{
+  public static class ControllerExtensions
+  {
+    public static IHttpActionResult TryResult(this ApiController controller, Action fn)
+    {
+      try
+      {
+        fn();
+      }
+      catch (Exception e)
+      {
+        return new ExceptionResult(e, controller);
+      }
+
+      return new OkResult(controller);
+    }
+  }
+}

363d3de47e5678c4cb991c93cb4e0b91ae231525
Philip Cooper <phillycooper@hotmail.com>
2020-10-06 21:12:01 +0100

    Create IEntityController.cs

---
+++ src/Controllers/IEntityController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web.Http;
+
+namespace restlessmedia.Module.Web.Api.Controllers
+{
+  [RoutePrefix("api/entity")]
+  public class IEntityController : ApiController
+  {
+    public IEntityController(IEntityService entityService)
+    {
+      _entityService = entityService ?? throw new ArgumentNullException(nameof(entityService));
+    }
+
+    [HttpPost]
+    [Route("{source}/{sourceId}/move/{target}/{targetId}/{direction}")]
+    public IHttpActionResult Move(EntityType source, int sourceId, EntityType target, int targetId, MoveDirection direction)
+    {
+      try
+      {
+        _entityService.Move(source, target, sourceId, targetId, direction);
+        return Ok();
+      }
+      catch (Exception e)
+      {
+        return InternalServerError(e);
+      }
+    }
+
+    [HttpPost]
+    [Route("{source}/{sourceId}/move/{targetId}/{direction}")]
+    public IHttpActionResult Move(EntityType source, int sourceId, int targetId, MoveDirection direction)
+    {
+      return Move(source, sourceId, source, targetId, direction);
+    }
+
+    private readonly IEntityService _entityService;
+  }
+}
